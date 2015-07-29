4e9e52f36497f185db44b2d4f65e36f2f5145aad
jwalkie <jwalkk@umich.edu>
2015-07-29 09:33:33 +0200

    deploying to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150728121349) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "deals", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "city_planner"
@@ -93,7 +96,7 @@ ActiveRecord::Schema.define(version: 20150728121349) do
     t.string   "account_name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
