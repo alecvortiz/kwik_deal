9348497f21b30484cdb6c074d4b8c949b1d678b4
alecvortiz <aortiz2@stanford.edu>
2015-08-03 12:02:18 +0000

    fixed search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,7 +43,7 @@ class ProductsController < ApplicationController
   
   private
   def product_params 
-    params.require(:product).permit(:deal_id, :option_name, :expiration_type, :months_to_expiration, :days_to_expiration, :unit_value, :unit_sell_price, :option_commission, :buyer_max, :option_cap, 
+    params.require(:product).permit(:deal_id, :option_name, :expiration_type, :months_to_expiration, :days_to_expiration, :unit_value, :unit_sell_price, :unit_buy_price, :option_commission, :buyer_max, :option_cap, 
     :display_order, :sell_number, :units_per_carton, :cartons_per_pallet, :pallets_stackable, :est_shipping_cost, :country_origin, :dimensions_uom, 
     :weight_uom, :retail_package_height, :retail_package_length, :retail_package_weight, :retail_package_width, :master_carton_height, :master_carton_length, 
     :master_carton_weight, :master_carton_width, :avatar)
